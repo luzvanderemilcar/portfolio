fc8386fc23da16e4fabc553db5ee5c5b5b188a1f
Luzvander EMILCAR <luzvanderemilcar@gmail.com>
2025-03-24 13:37:34 -0400

    update "set active link element on scrollend"

--- main.js
+++ main.js
@@ -23,8 +23,10 @@ function gererEntreeVide(e) {
   }
 }
 
-// liensInternes
+// liens internes et target internes
 const liensInternes = document.querySelectorAll(".internal-link");
+const documentSections = document.querySelectorAll(".internal-target");
+
 
 // ajouter un listener pour les clicks sur les liens internes
 liensInternes.forEach(elementLien => {
@@ -43,18 +45,108 @@ function gererClickLienInterne(e) {
   // défiler la fenêtre vers la position ciblée
   window.scrollTo({ top: targetPosition, behavior: 'smooth' });
   // set active
-  removeNavBarActive();
-  addNavBarActive(targetSelector.substring(1));
+  setActive(targetSelector.substring(1))
   
 }
 
+
 function removeNavBarActive() {
-  let navBarLinks = document.querySelectorAll("a.internal-link");
+  let navBarLinks = document.querySelectorAll(".navbar-link");
   navBarLinks.forEach(link => {
+    // remove active class from the active element
     link.classList.contains('active') && link.classList.remove('active');
   })
 }
 
 function addNavBarActive(id) {
- document.querySelector(`[href="#${id}"]`).classList.add("active");
+  let targetElement = document.querySelector(`[href="#${id}"]`);
+  !targetElement.classList.contains('active') && targetElement.classList.add("active");
+}
+
+function setActive(id) {
+  // remove current active
+  removeNavBarActive();
+  // add active to element with href id
+  addNavBarActive(id);
+}
+
+let headerElement = document.querySelector("#contact");
+
+// Sections 
+let tTimeout;
+let scrollDelaySeconds = .5;
+
+document.addEventListener("scrollend", (e) => {
+  // debounce the scrollend actions for the active element setting
+  if (tTimeout) clearTimeout(tTimeout);
+  tTimeout = setTimeout(() => {
+    // find the active section at scrollend
+    let activeSection = getActiveElement(documentSections,  getHeaderOffset());
+    let activeSectionId = activeSection.getAttribute('id');
+  
+  setActive(activeSectionId)
+  }, scrollDelaySeconds * 1000);
+});
+
+// find the active element prioritizing the element whose top is inside the viewport
+function getActiveElement(elements, topOffsetHeight) {
+  let activeElement;
+  let index = 0;
+  
+  // tester l'element dont le top se trouve dans le viewport
+  while (!activeElement && index < elements.length) {
+    if (isTopInView(elements[index], topOffsetHeight)) {
+      activeElement = elements[index];
+    }
+    index++;
+  }
+  // tester si aucun des elements ne se commence dans le viewport
+  if (!activeElement) {
+    index = 0;
+    // tester l'element dont le corps se trouve dans le viewport
+    while (!activeElement && index < elements.length) {
+      if (isBodyInView(elements[index], topOffsetHeight)) {
+        activeElement = elements[index];
+      }
+      index++;
+    }
+  }
+  // return l'attribut id de l'element active
+  return activeElement;
+}
+
+
+// find if an element's top is currently inside the viewport 
+function isTopInView(element, topOffsetHeight = 0) {
+  let { height, width, y: distanceFromTop, x: distanceFromLeft } = getElementPositions(element);
+  // ratio of the height of the visible part of an element in the viewport for it to be considered as active
+  let ratioMinInViewToBeActive = .6;
+  
+  // the y (top start position) of the element is positive and greater than a specific top offset but fewer than the height of the viewport 
+  if (distanceFromTop >= topOffsetHeight && distanceFromTop < window.innerHeight * (1 - ratioMinInViewToBeActive) ) {
+    if (distanceFromLeft < window.innerWidth || (distanceFromLeft + width) > 0) {
+      return true;
+    }
+  }
+  // default return 
+  return false;
+}
+
+// find if an element's body is currently inside the viewport 
+function isBodyInView(element, topOffsetHeight = 0) {
+  let { height, width, y: distanceFromTop, x: distanceFromLeft } = getElementPositions(element);
+  
+  // the y of the element is negative or fewer than a specific top offset 
+  if (distanceFromTop < topOffsetHeight && (distanceFromTop + height) > topOffsetHeight) {
+    if (distanceFromLeft < window.innerWidth || (distanceFromLeft + width) > 0) {
+      return true;
+    }
+  }
+  // default return 
+  return false;
+}
+// find the positions of an element
+function getElementPositions(element) {
+  let elementClientRects = element.getClientRects()[0];
+  return elementClientRects;
 }
